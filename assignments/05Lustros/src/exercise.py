5e147b4e6bd1e87074600c24095fabec7a9b9ecd
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-08-23 21:20:20 -0500

    Update exercise.py

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+    n=(int(input("Insert your birth date: ")))
+a=(int(input("Insert the actual year: ")))
+lusters=(float((a-n)/5))
+print("The lusters you have lived are: ",lusters)
     pass
 
 
