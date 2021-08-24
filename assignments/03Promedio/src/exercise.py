ccf271f71544f58d2352fd2a0e51fe9232d2dd12
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-08-23 20:37:53 -0500

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
+    materia1=float(input("Insert the first grade: "))
+materia2=float(input("Insert the second grade: "))
+materia3=float(input("Insert the third grade: "))
+materia4=float(input("Insert the fourth grade: "))
+average=(materia1+materia2+materia3+materia4)/4
+print("The average is: ",average)
     pass
 
 
