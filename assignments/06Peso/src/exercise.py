7ebb6a9ec9e6615780132fc012e73b0733584fb3
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-08-23 21:51:06 -0500

    Update exercise.py

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
+    pactual=(float(input("Insert your actual weight: ")))
+pdeseado=(float(input("Insert your desired weight: ")))
+meses=(int(input("Insert the months you'll be on the program: ")))
+kgporbajar=(float((pactual-pdeseado)/meses))
+print("Weight you have to loose per month: ",kgporbajar)
     pass
     
 
